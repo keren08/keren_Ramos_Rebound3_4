b13be2f88224e4ffe4d5d505c09ce22d72b4a598
keren Ramos <keren.ramoss@gmail.com>
2022-12-12 12:50:47 -0600

    validarcion

--- js/index.js
+++ js/index.js
@@ -1,31 +1,46 @@
-const borrar = document.getElementById('cancelar');
-const enviar = document.getElementById('enviar');
-const contenedor = document.getElementById('contenedor');
-const formContenedor = document.getElementById('formContenedor');
+const borrar = document.getElementById("cancelar");
+const enviar = document.getElementById("enviar");
+const contenedor = document.getElementById("contenedor");
+const form = document.getElementById("form");
 
+//informacion del formulario
+const numeroT = document.getElementById("numeroT");
+const nombre = document.getElementById("nombre");
+const fecha = document.getElementById("fecha");
+const cvs = document.getElementById("cvs");
 
-
-borrar.addEventListener('click',(e)=>{
-    e.preventDefault();
-    if(document.getElementById('formContenedor')){
+borrar.addEventListener("click", (e) => {
+  e.preventDefault();
+  const formContenedor = document.getElementById("formContenedor");
+  if (formContenedor) {
     formContenedor.remove();
-    contenedor.insertAdjacentHTML('beforeend',agregarRojo());
-    borrar.classList.add('botonRojo');    
+    contenedor.insertAdjacentHTML("beforeend", agregarRojo());
+    borrar.classList.add("botonRojo");
+  }
+});
+
+enviar.addEventListener("click", (e) => {
+  e.preventDefault();
+  const formContenedor = document.getElementById("formContenedor");
+  let validacion = validaciones(
+    numeroT.value,
+    nombre.value,
+    fecha.value,
+    cvs.value
+  );
+  if (formContenedor) {
+    if (validacion) {
+      formContenedor.remove();
+      contenedor.insertAdjacentHTML("beforeend", agregarAzul());
+      enviar.classList.add("botonAzul");
+    } else {
+      alert("todos los campos deben llenarse");
     }
+  }
 });
 
-enviar.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    if(document.getElementById('formContenedor')){
-    formContenedor.remove();
-    contenedor.insertAdjacentHTML('beforeend',agregarAzul());
-    enviar.classList.add('botonAzul');    
-    
-}
-});
-
-function agregarRojo(){
-    const html = `
+function agregarRojo() {
+  const html = `
     <div id="color__rojo" class="color__rojo ">
     <img
       src="assets/img/equis.png"
@@ -36,8 +51,8 @@ function agregarRojo(){
   return html;
 }
 
-function agregarAzul(){
-    const html = `
+function agregarAzul() {
+  const html = `
     <div id="color__blue" class="color__blue">
     <img
       src="assets/img/check.png"
@@ -46,4 +61,12 @@ function agregarAzul(){
     />
   </div>`;
   return html;
+}
+
+function validaciones(numeroT, nombre, fecha, cvs) {
+  if (numeroT && nombre && fecha && cvs) {
+    return true;
+  } else {
+    return false;
+  }
 }
